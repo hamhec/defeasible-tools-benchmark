ab860dc2c4bfec770064840ef2c11449a1b8e093
Abdelraouf Hecham <hecham.abdelraouf@gmail.com>
2017-07-04 18:03:26 +0200

    Initial commit

---
+++ src/main/java/fr/lirmm/graphik/defeasible/tools/benchmark/App.java
@@ -0,0 +1,13 @@
+package fr.lirmm.graphik.defeasible.tools.benchmark;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
