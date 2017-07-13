467b2c17555c63e48568afa95807d061ded423a1
Abdelraouf Hecham <hecham.abdelraouf@gmail.com>
2017-07-13 21:59:30 +0200

    Coded Abstract Tool

--- src/main/java/fr/lirmm/graphik/defeasible/tools/benchmark/tools/AbstractTool.java
+++ src/main/java/fr/lirmm/graphik/defeasible/tools/benchmark/tools/AbstractTool.java
@@ -1,6 +1,8 @@
 package fr.lirmm.graphik.defeasible.tools.benchmark.tools;
 
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.apache.commons.lang3.tuple.Pair;
 
@@ -10,26 +12,57 @@ import fr.lirmm.graphik.defeasible.core.rules.DefeasibleRule;
 import fr.lirmm.graphik.defeasible.core.rules.DefeaterRule;
 import fr.lirmm.graphik.defeasible.tools.benchmark.core.Approach;
 import fr.lirmm.graphik.graal.api.core.Atom;
+import fr.lirmm.graphik.graal.api.core.NegativeConstraint;
 import fr.lirmm.graphik.graal.api.core.Rule;
+import fr.lirmm.graphik.util.profiler.CPUTimeProfiler;
 import fr.lirmm.graphik.util.profiler.Profiler;
+import fr.lirmm.graphik.util.stream.CloseableIterator;
+import fr.lirmm.graphik.util.stream.IteratorException;
 
 public abstract class AbstractTool implements Approach {
 	
-	Profiler profiler;
-
-	public void run() {
-		// TODO Auto-generated method stub
-		
-	}
-
+	private Profiler profiler;
+	private StringBuilder KBStringBuilder;
+	private List<Pair<String, ? extends Object>> results; 
+	
+	public abstract String getName();
+	public abstract String formatStrictRule(Rule rule);
+	public abstract String formatDefeasibleRule(DefeasibleRule rule);
+	public abstract String formatDefeaterRule(DefeaterRule rule);
+	public abstract String formatFact(Atom atom);
+	public abstract String formatNegativeConstraint(NegativeConstraint nc);
+	public abstract String formatPreference(Preference pref);
+	
+	
 	public void prepare(DefeasibleKnowledgeBase kb) {
-		// TODO Auto-generated method stub
+		// Format Facts
+		try {
+			this.formatFacts(kb.getFacts().iterator());
+		} catch (IteratorException e1) {
+			e1.printStackTrace();
+		}
 		
+		// Format Rules
+		this.formatStrictRules(kb.getStrictRules().iterator());
+		this.formatDefeasibleRules(kb.getDefeasibleRules().iterator());
+		this.formatDefeaterRules(kb.getDefeaterRules().iterator());
+	
+		// Format Negative Constraints
+		this.formatNegativeConstraints(kb.getNegativeConstraints().iterator());
+		
+		// Format Preferences
+		try {
+			this.formatPreferences(kb.getPreferences().iterator());
+		} catch (IteratorException e) {
+			e.printStackTrace();
+		}
 	}
-
+	
+	// User initialize rather than the constructor because we need to do it for each iteration
 	public void initialize() {
-		// TODO Auto-generated method stub
-		
+		this.profiler = new CPUTimeProfiler();
+		this.results = new LinkedList<Pair<String, ? extends Object>>();
+		this.KBStringBuilder = new StringBuilder();
 	}
 
 	public Iterator<Pair<String, ? extends Object>> getResults() {
@@ -41,13 +74,45 @@ public abstract class AbstractTool implements Approach {
 		return this.profiler;
 	}
 	
-	public abstract String getName();
-	public abstract String formatStrictRule(Rule rule);
-	public abstract String formatDefeasibleRule(DefeasibleRule rule);
-	public abstract String formatDefeaterRule(DefeaterRule rule);
-	public abstract String formatFact(Atom atom);
-	public abstract String formatNegativeConstraint();
-	public abstract String formatPreference(Preference pref);
+	public void formatStrictRules(Iterator<Rule> itRules) {
+		while(itRules.hasNext()) {
+			this.KBStringBuilder.append(this.formatStrictRule(itRules.next()));
+			this.KBStringBuilder.append("\n");
+		}
+	}
+	public void formatDefeasibleRules(Iterator<Rule> itRules) {
+		while(itRules.hasNext()) {
+			this.KBStringBuilder.append(this.formatDefeasibleRule((DefeasibleRule) itRules.next()));
+			this.KBStringBuilder.append("\n");
+		}
+	}
+	public void formatDefeaterRules(Iterator<Rule> itRules) {
+		while(itRules.hasNext()) {
+			this.KBStringBuilder.append(this.formatDefeaterRule((DefeaterRule) itRules.next()));
+			this.KBStringBuilder.append("\n");
+		}
+	}
+	
+	public void formatNegativeConstraints(Iterator<Rule> itRules) {
+		while(itRules.hasNext()) {
+			this.KBStringBuilder.append(this.formatNegativeConstraint((NegativeConstraint) itRules.next()));
+			this.KBStringBuilder.append("\n");
+		}
+	}
+	
+	public void formatFacts(CloseableIterator<Atom> it) throws IteratorException {
+		while(it.hasNext()) {
+			this.KBStringBuilder.append(this.formatFact(it.next()));
+			this.KBStringBuilder.append("\n");
+		}
+	}
+	
+	public void formatPreferences(Iterator<Preference> it) throws IteratorException {
+		while(it.hasNext()) {
+			this.KBStringBuilder.append(this.formatPreference(it.next()));
+			this.KBStringBuilder.append("\n");
+		}
+	}
 	
 	
 }
--- src/main/java/fr/lirmm/graphik/defeasible/tools/benchmark/tools/ELDRTool.java
+++ src/main/java/fr/lirmm/graphik/defeasible/tools/benchmark/tools/ELDRTool.java
@@ -1,5 +1,52 @@
 package fr.lirmm.graphik.defeasible.tools.benchmark.tools;
 
-public class ELDRTool {
+import fr.lirmm.graphik.defeasible.core.preference.Preference;
+import fr.lirmm.graphik.defeasible.core.rules.DefeasibleRule;
+import fr.lirmm.graphik.defeasible.core.rules.DefeaterRule;
+import fr.lirmm.graphik.graal.api.core.Atom;
+import fr.lirmm.graphik.graal.api.core.Rule;
+
+public class ELDRTool extends AbstractTool {
+	public static final String NAME = "ELDR";
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	@Override
+	public String formatStrictRule(Rule rule) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String formatDefeasibleRule(DefeasibleRule rule) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String formatDefeaterRule(DefeaterRule rule) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String formatFact(Atom atom) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String formatNegativeConstraint() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String formatPreference(Preference pref) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
