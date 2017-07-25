e481f42f6d0656e6e2329c1cf4552347b9281b1c
Abdelraouf Hecham <hecham.abdelraouf@gmail.com>
2017-07-25 11:24:21 +0200

    ASPIC Tool: Formatting Unitest Except Preferences

---
+++ src/test/java/fr/lirmm/graphik/defeasible/tools/benchmark/tools/ASPICToolTest.java
@@ -0,0 +1,52 @@
+package fr.lirmm.graphik.defeasible.tools.benchmark.tools;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import fr.lirmm.graphik.defeasible.core.io.DlgpDefeasibleParser;
+import fr.lirmm.graphik.defeasible.core.preference.Preference;
+import fr.lirmm.graphik.defeasible.core.rules.DefeasibleRule;
+import fr.lirmm.graphik.defeasible.core.rules.DefeaterRule;
+import fr.lirmm.graphik.graal.api.core.Atom;
+import fr.lirmm.graphik.graal.api.core.NegativeConstraint;
+import fr.lirmm.graphik.graal.api.core.Rule;
+import fr.lirmm.graphik.graal.api.io.ParseException;
+
+public class ASPICToolTest {
+	public static ASPICTool tool = new ASPICTool();
+	
+	@Test
+	public void shouldFormatGroundAtom() throws ParseException {
+		Atom atom = DlgpDefeasibleParser.parseAtom("p(a).");
+		Assert.assertTrue((tool.formatFact(atom).equals("p(a).")));
+	}
+	@Test
+	public void shouldFormatGroundAtomWithArity() throws ParseException {
+		Atom atom = DlgpDefeasibleParser.parseAtom("p(a,b).");
+		Assert.assertTrue((tool.formatFact(atom).equals("p(a,b).")));
+	}
+	
+	@Test
+	public void shouldFormatStrictRuleWithLabel() throws ParseException {
+		Rule rule = DlgpDefeasibleParser.parseRule("[r1] p(X) <- q(X).");
+		Assert.assertTrue((tool.formatStrictRule(rule).equals("p(X) <- q(X) .\n")));
+	}
+	@Test
+	public void shouldFormatStrictRuleWithoutLabel() throws ParseException {
+		Rule rule = DlgpDefeasibleParser.parseRule("p(X) <- q(X).");
+		Assert.assertTrue((tool.formatStrictRule(rule).equals("p(X) <- q(X) .\n")));
+	}
+	@Test
+	public void shouldFormatDefeasibleRuleWithLabel() throws ParseException {
+		DefeasibleRule rule = DlgpDefeasibleParser.parseDefeasibleRule("[r1] p(X) <= q(X).");
+		Assert.assertTrue((tool.formatDefeasibleRule(rule).equals("p(X) <- q(X) "+ tool.DEFEASIBLE + ".\n")));
+	}
+	
+	@Test
+	public void shouldFormatNegativeConstraint() throws ParseException {
+		NegativeConstraint nc = DlgpDefeasibleParser.parseNegativeConstraint("! :- p(X), q(X).");
+		Assert.assertTrue((tool.formatNegativeConstraint(nc).equals("~p(X) <- q(X).\n~q(X) <- p(X).")));
+	}
+	
+
+}
